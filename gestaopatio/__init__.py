ad5ff2201beb3d6e761d8a5281fae6dbfdb4a055
Marcbety <marcbety@yahoo.com.br>
2025-06-10 14:38:05 -0300

    Update __init__.py

--- gestaopatio/__init__.py
+++ gestaopatio/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from flask_sqlalchemy import SQLAlchemy
- 
+import os
 
 app = Flask(__name__)
     
     
 app.config['SECRET_KEY'] = 'ed9af17758e577d8e0aaa8f22a87b6bbf4ead56536'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///agendamento.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI']=os.getenv("DATABASE_URL")
+else:    
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///agendamento.db'
 app.config['WTF_CSRF_ENABLED'] = True
     
 database = SQLAlchemy(app)
