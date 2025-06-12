550da3f5178ee6f5304d11d262346c73cb913478
Marcbety <marcbety@yahoo.com.br>
2025-06-12 11:18:06 -0300

    Update __init__.py

--- gestaopatio/__init__.py
+++ gestaopatio/__init__.py
@@ -20,4 +20,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-from gestapatio import routes
+from gestapatiokoandina import routes
