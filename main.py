010305e7fae0d3cd108af42ace9b618f12ad3378
Marcbety <marcbety@yahoo.com.br>
2025-06-27 16:26:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from gestaopatio import app, database  
- # Crie o banco de dados dentro do contexto da aplicação
+import logging
+from logging.handlers import RotatingFileHandler
+from gestaopatio import app, database
+
+# Cria o banco de dados dentro do contexto da aplicação
 with app.app_context():
-       #database.drop_all()
-       database.create_all()
-    
-    
-    
+    # database.drop_all()  # Use com cuidado!
+    database.create_all()
+
 if __name__ == '__main__':
-        handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=1)
-        handler.setLevel(logging.ERROR)
-        app.logger.addHandler(handler)
-        app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False, use_debugger=False)
+    # Configura o log de erros
+    handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.ERROR)
+    app.logger.addHandler(handler)
+
+    # Inicia o servidor Flask
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
