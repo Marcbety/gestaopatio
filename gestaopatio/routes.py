2894b8d8697d9b7a1f7d05920686f0a8d41c4121
Marcbety <marcbety@yahoo.com.br>
2025-07-18 09:58:30 -0300

    Update routes.py

--- gestaopatio/routes.py
+++ gestaopatio/routes.py
@@ -586,18 +586,24 @@ def entidade():
     
     
     
+
 @app.route('/check_in/', methods=['GET', 'POST'])
 def check_in():
-        agendamento_id = request.args.get('agendamento_id', type=int)
-        agendamentos_c = Agendamentos.query.get_or_404(agendamento_id)
-        utc_now = datetime.utcnow()
-        local_tz = pytz.timezone('America/Sao_Paulo')  # Ajuste para o seu fuso horário local
-        local_now = utc_now.replace(tzinfo=pytz.utc).astimezone(local_tz)
-        agendamentos_c.check_in = local_now
-        agendamentos_c.fase_carga = "CHECK-IN"
-        database.session.commit()
-        flash('Check-In realizado com sucesso!', 'alert-success')
-        return redirect(url_for('painel'))
+    agendamento_id = request.args.get('agendamento_id', type=int)
+    agendamentos_c = Agendamentos.query.get_or_404(agendamento_id)
+
+    utc_now = datetime.utcnow()
+    utc_aware = pytz.utc.localize(utc_now)  # Corrige o timezone
+    local_tz = pytz.timezone('America/Sao_Paulo')
+    local_now = utc_aware.astimezone(local_tz)
+
+    agendamentos_c.check_in = local_now
+    agendamentos_c.fase_carga = "CHECK-IN"
+    database.session.commit()
+
+    flash('Check-In realizado com sucesso!', 'alert-success')
+    return redirect(url_for('painel'))
+
     
 @app.route('/entrada_patio/', methods=['GET', 'POST'])
 def entrada_patio():
