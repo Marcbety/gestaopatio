eedf6ae8ccb545cbe6aec812c193baecb9058dd7
Marcbety <marcbety@yahoo.com.br>
2025-07-29 16:30:33 -0300

    Update routes.py

--- gestaopatio/routes.py
+++ gestaopatio/routes.py
@@ -78,7 +78,7 @@ def atualizar_gnre():
 
 @app.route('/painel_rota')
 def painel_rota():
-    lista_patio = Control_Patio.query.filter(Control_Patio.num_frota == '710').order_by(Control_Patio.num_doca).all()
+    lista_patio = Control_Patio.query.filter(Control_Patio.num_frota == '704').order_by(Control_Patio.num_doca).all()
     
     for controle in lista_patio:
         num_transporte = controle.num_transporte
